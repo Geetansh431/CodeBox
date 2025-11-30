c31cc27187658d4d5140cbf2e00dc1d541fa7189
geetansh431 <geetanshg2@gmail.com>
2025-11-30 16:17:48 +0530

    feat: Setup Dark Theme as Default Theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Pixelify_Sans, Inter } from "next/font/google";
 import "./globals.css";
+import { Provider } from "../app/provider"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,11 +34,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning className="dark">
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${GameFont.variable} ${inter.variable} antialiased`}
       >
-        {children}
+        <Provider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </Provider>
       </body>
     </html>
   );
---
+++ app/provider.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { ThemeProvider as NextThemesProvider } from 'next-themes'
+
+export function Provider({
+    children,
+    ...props
+    } : React.ComponentProps<typeof NextThemesProvider>) {
+    return (
+        <NextThemesProvider {...props}>
+           {children}
+        </NextThemesProvider>
+    )
+}
