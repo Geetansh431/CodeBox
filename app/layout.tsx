bd8557dd7f571b6c30b6d2a04dbb12865c3b01b7
geetansh431 <geetanshg2@gmail.com>
2025-12-01 23:33:19 +0530

    feat: Setup Clerk Provider and Middleware

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Pixelify_Sans, Inter, Jersey_10 } from "next/font/google";
 import "./globals.css";
 import { Provider } from "../app/provider"
+import { ClerkProvider } from '@clerk/nextjs'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,6 +36,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
+  
     <html lang="en" suppressHydrationWarning className="dark">
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${GameFont.variable} ${inter.variable} antialiased`}
@@ -49,5 +52,7 @@ export default function RootLayout({
         </Provider>
       </body>
     </html>
+
+    </ClerkProvider>
   );
 }
