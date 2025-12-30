f05a987a55ca3f2c4f14fcc86fad24dcc0183859
geetansh431 <geetanshg2@gmail.com>
2025-12-30 09:34:55 +0530

    feat: Add UserDetailContext for managing user data globally

--- app/provider.tsx
+++ app/provider.tsx
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
 import { useUser } from '@clerk/nextjs'
 import axios from 'axios'
+import { UserDetailContext } from '@/context/UserDetailContext'
 
 export function Provider({
     children,
@@ -10,18 +11,22 @@ export function Provider({
     } : React.ComponentProps<typeof NextThemesProvider>) {
 
     const {user} = useUser();
-    
+    const [userDetail, setUserDetail] = React.useState();
+
     useEffect(() => {
         user && CreateNewUser();
     }, [user]);
 
     const CreateNewUser = async () => {
         const result = await axios.post('/api/user',{});
+        setUserDetail(result?.data);
     }
 
     return (
         <NextThemesProvider {...props}>
-           {children}
+            <UserDetailContext.Provider value={{ userDetail, setUserDetail }}>
+                {children}
+            </UserDetailContext.Provider>
         </NextThemesProvider>
     )
 }
